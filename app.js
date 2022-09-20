6414ec807134a4731e79fe56a04e27834b7017a0
Janette <janette.castillo91@yahoo.com>
2022-09-20 15:10:02 -0500

    declared variables

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-document.addEventListener('DOMContentLoaded' () => {
+document.addEventListener('DOMContentLoaded', () => {
+const grid = docuemnt.querySelector('.grid')
+const squares = Array.from(document.querySelectorAll('.grid div'))
+const width = 10
 
+console.log(squares)
 
-
-    
 })
