03abaf180c6440d3112b8d097775797f517d58b8
Janette <janette.castillo91@yahoo.com>
2022-09-20 15:52:26 -0500

    added shapes to an array

--- app.js
+++ app.js
@@ -5,38 +5,40 @@ document.addEventListener("DOMContentLoaded", () => {
   const StartBtn = document.querySelector("#.start0b");
   const width = 10;
 
-  // tetrominoes
-  const ltetromino = [
+  // shapes
+  const lShape = [
     [1, width + 1, width * 2 + 1, 2],
     [width, width + 1, width + 2, width * 2 + 2],
     [1, width + 1, width * 2 + width * 2],
     [width, width * 2, width * 2 + 1, width * 2 + 2],
   ];
-  const sTetromino = [
+  const sShape = [
     [0, width, width + 1, width * 2 + 1],
     [width + 1, width + 2, width * 2, width * 2 + 1],
     [0, width, width + 1, width * 2 + 1],
     [width + 1, width + 2, width * 2, width * 2 + 1],
   ];
 
-  const tTetromino = [
+  const tShape= [
     [1, width, width + 1, width + 2],
     [1, width + 1, width + 2, width * 2 + 1],
     [width, width + 1, width + 2, width * 2 + 1],
     [1, width, width + 1, width * 2 + 1],
   ];
 
-  const oTetromino = [
+  const oShape = [
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
   ];
 
-  const iTetromino = [
+  const iShape = [
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3],
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3],
   ];
+
+  const theShapes = [iShape, sShape, tShape, oShape, iShape]
 });
